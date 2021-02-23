9828ef79ad0e984b3dbdabbc511f1f699d6c5d99
kevin <897745959@qq.com>
2021-02-23 23:48:19 +1100

    login ui complete

---
+++ js/login.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.login_log .link_reg a').click(function() {
+        $('.login_log').hide();
+        $('.login_reg').show();
+        
+    })
+
+
+    $('.login_reg .link_log a').click(function() {
+        $('.login_log').show();
+        $('.login_reg').hide();
+        
+    })
+
+
+
+
+
+
+})
